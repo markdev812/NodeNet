480d508396b5044aa05d6c2e7c72f60476f159f5
markdev812 <mahogland@gmail.com>
2016-12-07 15:53:45 -0600

    initial

---
+++ Node.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Deployment.Application;
+
+
+namespace NodeNet
+{
+	public class Node
+	{
+		private float _value;
+		Dictionary<float,float> _state = new Dictionary<float, float>();
+
+		void Update()
+		{
+			
+		}
+
+		
+	}
+}
